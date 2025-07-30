bf2ef6fae7637425ebcd3a6aa4740d5570e49a19
Karam Khasawneh <202410447@std.jadara.edu.jo>
2025-07-30 21:56:44 +0300

    Update AppContext.js

--- frontend/src/contexts/AppContext.js
+++ frontend/src/contexts/AppContext.js
@@ -74,8 +74,7 @@ export const AppProvider = ({ children }) => {
       } catch (error) {
         console.error("Chatbot Live Data Fetch Error:", error);
         setLiveData(null);
-      }
-    } finally {
+      } finally {
       setIsLoadingData(false);
     }
     fetchUserLiveData();
