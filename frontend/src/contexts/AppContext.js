49d1887c4925dc6829d3891fcc4d9a25a0a15349
Karam Khasawneh <202410447@std.jadara.edu.jo>
2025-07-30 05:14:53 +0300

    Update AppContext.js

--- frontend/src/contexts/AppContext.js
+++ frontend/src/contexts/AppContext.js
@@ -1,7 +1,11 @@
 import React, { createContext, useContext, useState, useEffect } from 'react';
 import { useLocation } from 'react-router-dom';
 import { useLanguage } from './LanguageContext';
+import { useLanguage } from './LanguageContext';
 
+console.log("AppContext file is running now!"); // <--- أضف هذا السطر هنا
+
+// ... باقي الكود
 const AppContext = createContext();
 export const useApp = () => useContext(AppContext);
 
