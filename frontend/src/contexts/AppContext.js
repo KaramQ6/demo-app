59f4f7ef4dd9cb30f97a108ca2f4125fe71c71e5
Karam Khasawneh <202410447@std.jadara.edu.jo>
2025-08-02 23:50:11 +0300

    Update AppContext.js

--- frontend/src/contexts/AppContext.js
+++ frontend/src/contexts/AppContext.js
@@ -1,7 +1,7 @@
 import React, { createContext, useContext, useState, useEffect } from 'react';
 import { useLanguage } from './LanguageContext';
 
-const AppContext = createContext();
+export const AppContext = createContext();
 export const useApp = () => useContext(AppContext);
 
 export const AppProvider = ({ children }) => {
